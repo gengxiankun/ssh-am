b408517ad21a7e24cd0a3653cde324044bc51973
xiankun.geng <ge2x3k2@gmail.com>
2017-07-15 15:14:52 +0800

    [update] 自动补全

--- ploo_complete.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#@auther <ge2x3k2@gmail.com>
-#@time 2017-07-15 01:27 pm
-
-complete_arr=`cat $1 | grep "#" | cut -d'[' -f 2 | cut -d']' -f 1`
-
-source complete -W "$complete_arr" ploo
